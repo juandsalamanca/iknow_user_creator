aabbb00a61251d7eede4e042c38bcd2c28c1d212
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-06-06 09:57:29 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import requests
+
+st.header("User creator")
+
+user = st.text_input("Type here your username")
+pwd = st.text_input("Type here your password")
+email = st.text_input("Type here your email")
+
+if user and pwd and email:
+  submit = st.button("Submit")
+  if submit:
+    payload = {"username":user, "password":pwd, "email":email}
+    response = requests.post("https://api.iknow.aidevlab.com/api/v1/auth/signup", json=payload)
+    if response.status_code == 200:
+      st.success("New user created!")
